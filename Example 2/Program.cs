6e19bc502fff406375f3d185f6f96e8d21fc93bd
darafomina <darafomina13403@gmail.com>
2022-08-15 17:03:15 +0300

    Home task

---
+++ Example 2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine ("Программа по нахождению третей цифры числа");
+
+Console.WriteLine ("Введите число");
+string? numberString = Console.ReadLine();
+int number = int.Parse(numberString!);
+
+if (number > 99)
+    Console.WriteLine("Третья цифра: " + number.ToString()[2]);
+else if  (number < -99)
+    Console.WriteLine("Третья цифра: " + number.ToString()[3]);
+else
+    Console.WriteLine("Третьей цифры нет");
---
+++ Example 3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.WriteLine ("Программа по нахождению выходного дня");
+
+Console.WriteLine ("Введите число");
+string? NumberString = Console.ReadLine ();
+int number = int.Parse (NumberString!);
+
+if(number >= 1 && number <= 5)
+{
+    Console.WriteLine("Будний день");
+}
+else if (number == 6 || number == 7)
+{
+    Console.WriteLine("Выходной день");
+}
+else
+Console.WriteLine("Введите цифру, соответствующую дню недели");
